eeba018978b9354ce6cb8e0c2fa0385c1a600411
Zander <94405288+Zmarenberg@users.noreply.github.com>
2022-12-16 22:08:37 -0500

    added functions to generate readme and filled in template literal

--- develop/index.js
+++ develop/index.js
@@ -3,79 +3,109 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
+const util = require('./utils/generateMarkdown');
 const path = require('path');
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
 
 // Array of questions for user input
-const questions = [
-  {// question object for github user name
-    type: 'input',
-    name: 'GitHub',
-    message: 'What is your GitHub Username?',
-  },
+inquirer
+  .prompt([
+    {// question object for github user name
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub Username?',
+    },
 
-  {// question object for email
-    type: 'input',
-    name: 'email',
-    message: 'What is your preferred email address?',
-  },
+    {// question object for email
+      type: 'input',
+      name: 'email',
+      message: 'What is your preferred email address?',
+    },
 
-  {// question object for project name
-    type: 'input',
-    name: 'project',
-    message: 'What is the title of your Project/Repo?',
-  },
+    {// question object for project name
+      type: 'input',
+      name: 'project',
+      message: 'What is the title of your Project/Repo?',
+    },
 
-  {// question object for the description
-    type: 'input',
-    name: 'descr',
-    message: 'Write a description for your project',
-  },
+    {// question object for the description
+      type: 'input',
+      name: 'descr',
+      message: 'Write a description for your project',
+    },
 
-  {// question object for license
-    type: 'list',
-    name: 'license',
-    message: 'Choose a License for your project',
-    choices: ['MIT', 'Mozilla Public License 2.0', 'Apache License 2.0', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3'],
-  },
+    {// question object for license
+      type: 'list',
+      name: 'license',
+      message: 'Choose a License for your project',
+      choices: ['MIT', 'Mozilla Public License 2.0', 'Apache License 2.0', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'No License'],
+    },
 
-  {// question object for the installation instruction with default
-    type: 'input',
-    name: 'test',
-    message: 'Please include any installation instructions if necessary',
-  },
+    {// question object for the installation instruction with default
+      type: 'input',
+      name: 'install',
+      message: 'Please include any installation instructions if necessary',
+    },
 
-  {// question object for test with default
-    type: 'input',
-    name: 'test',
-    message: 'Include any relevant test information/instructions',
-  },
+    {// question object for test with default
+      type: 'input',
+      name: 'test',
+      message: 'Include any relevant test information/instructions',
+    },
 
-  {// question object for usage of the repo
-    type: 'input',
-    name: 'usage',
-    message: 'Provide instructions of your project in use if applicable',
-  },
+    {// question object for usage of the repo
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions of your project in use if applicable',
+    },
 
-  {// question object for contributing to the repo
-    type: 'input',
-    name: 'contributions',
-    message: 'Provide details as to how other developers can contribute to your project',
-  },
-];
+    {// question object for contributing to the repo
+      type: 'input',
+      name: 'contributions',
+      message: 'Provide details as to how other developers can contribute to your project',
+    },
+  ])
+
+
+  .then((data) => {
+    const content = generateMarkdown(data);
+
+
+    fs.writeFile("ReadMe.MD", content, (err) =>
+      err ? console.log(err) : console.log("Nice Job!")
+    );
+  });
+
+
+// function init() {
+
+
+
+// }
+
+// init();
+
+
+
+
+
 
 // Function to write README file using the user input
-function writeToFile(fileName, data) {
+// TODO:
+// fs write function to create the README.md file
+// path package can be used to create the file path
+// function writeToFile(fileName, data) {
 
-  // TODO:
-  // fs write function to create the README.md file
-  // path package can be used to create the file path
 
-}
+//   fs.writeFile(fileName, data, (err) =>
+//     err ? console.log(err) : console.log("Nice Job!")
+//   )
+// };
 
 // Function to initialize app
-function init() {
+// function init() {
 
   // TODO:
   // call inquirer prompt method for questions
@@ -83,6 +113,6 @@ function init() {
   // call generateMarkdown passing answer object as input argument
   // call writeFile passing file name and the returned value from generateMarkdown function as input arguments
 
-}
+// }
 
-init();
+// init();
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,20 +1,50 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+const generateMarkdown = ({ github, email, project, descr, license, install, test, usage, contributions }) => {
+
+  return `# <${project}>
+
+## Description
+${descr}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${install}
+
+## Usage
+${usage}
+
+## License
+${license}
+
+## How to Contribute
+${contributions}
+
+## Tests
+${test}
+
+## Contact
+My GitHub username is: ${github}
+My email address is: ${email}
+`
 }
 
 module.exports = generateMarkdown;
