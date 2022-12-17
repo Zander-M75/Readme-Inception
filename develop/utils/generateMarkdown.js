c83330ff43ed1ee31ba1aab8a7b90377f0772ceb
Zander <94405288+Zmarenberg@users.noreply.github.com>
2022-12-16 22:39:19 -0500

    included license button and links

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,14 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  let badge = "";
+
+  if (license === 'MIT' || 'Mozilla Public License 2.0' || 'Apache License 2.0' || 'GNU AGPLv3', 'GNU GPLv3' || 'GNU LGPLv3') {
+    badge = `https://shields.io/badge/license-${license}-green`
+  }
+  return badge
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+
+  let link;
+  switch (license) {
+    case "MIT":
+      link = "https://mit-license.org/";
+      break;
+    case "Mozilla Public License 2.0":
+      link = "https://www.mozilla.org/en-US/MPL/2.0/";
+      break;
+    case "Apache License 2.0":
+      link = "https://www.apache.org/licenses/LICENSE-2.0";
+      break;
+    case "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3":
+      link = "https://www.gnu.org/licenses/agpl-3.0.en.html"
+      break;
+    default:
+      link = "";
+      break;
+  }
+
+  return link;
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  let section = "";
+  if (license === 'MIT' || 'Mozilla Public License 2.0' || 'Apache License 2.0' || 'GNU AGPLv3', 'GNU GPLv3' || 'GNU LGPLv3') {
+    section += "## License\n"
+  }
+}
+
+
+
+
 
 // TODO: Create a function to generate markdown for README
 
@@ -42,8 +82,7 @@ ${contributions}
 ${test}
 
 ## Contact
-My GitHub username is: ${github}
-My email address is: ${email}
+My GitHub username is: ${github} \nMy email address is: ${email}
 `
 }
 
